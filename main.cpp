5d596ce8c164e7df796e5dc30c7878e7e4217fa5
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-13 21:35:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int h;
+int w;
+int age;
+
 void getHeight(){
-	int h;
 	cout << "Please enter your Height in inches: " ;
 	cin >> h;
 }
 
 void getWeight() {
-	int w;
 	cout << "Please enter your Weight in pounds: " ;
 	cin >> w;
 }
 
 void getAge(){
-	int age;
 	cout << "Please enter your Age in years: " ;
 	cin >> age;
 }
 
-
+void hatsize(){
+	double hat = (w/h)*(2.9);
+	cout << "Your hat size is: " << hat << endl;
+}
 
 int main(){
 	getHeight();
 	getWeight();
 	getAge();
+	
+	hatsize();
 	
 	
 	
